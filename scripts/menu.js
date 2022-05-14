37c9d7e93b60d71f3ff5afb4d5707b4f76c594da
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2022-05-14 22:12:57 +0200

    Add files via upload
    Реализована логика для бургер-меню. Осталось разобраться как убирать открытое подменю при нажатии на другой пункт меню.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,26 @@
-let contextMenu = document.querySelectorAll('.link_show-more');
+let contextMenu = document.querySelectorAll('.link__show-more');
 
-let contextMenu_length = contextMenu.length;
+console.log('menu ',contextMenu);
 
-contextMenu.forEach(elem => {
+/*contextMenu.forEach(elem => {
     elem.addEventListener( "click" , () => {
         elem.children[1].classList.toggle("active");
-        
     });    
-});
+});*/
+
+for(let i=0; i<contextMenu.length; i++) {
+    contextMenu[i].addEventListener("click", ()=> {
+        contextMenu[i].children[1].classList.toggle("active");
+    })
+}
 
 
 /*Burger*/
 let burger = document.querySelector('#burger');
 
 burger.addEventListener( "click" , () => {
-        //elem.children[1].classList.toggle("active");
-        console.log('click');
+        let menu = document.querySelectorAll('.header__nav');
+        let register = document.querySelectorAll('.header__login');
+        menu[0].classList.toggle("active");
+        register[0].classList.toggle("active");
     });    
