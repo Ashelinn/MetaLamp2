183facc4219639cae98fc29d263599cab9af811d
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2022-11-11 18:34:02 +0200

    добавлен блок выбора кроватей и услуг в номере

--- source/scripts/forms.js
+++ source/scripts/forms.js
@@ -9,17 +9,17 @@ guestBtn.addEventListener('click', () => {
 })
 
 //+- logic
-function addGuest(tag) {
+function addGuest(tag,n) {
     let val = tag.innerHTML;
     let num = Number(val);
-    if(num < 5) num++
+    if(num < n) num++
     return num;
 }
 
-function delGuest(tag) {
+function delGuest(tag,n) {
     let val = tag.innerHTML;
     let num = Number(val);
-    if(num > 0) num--
+    if(num > n) num--
     return num;
 }
 
@@ -27,14 +27,14 @@ function delGuest(tag) {
 let adultsMinus = document.querySelector('#adults-minus');    
 adultsMinus.addEventListener('click', () => {
     let adults = document.querySelector('#adults');
-    let res = delGuest(adults);
+    let res = delGuest(adults,1);
     adults.innerHTML = res;
 });
 
 let adultsPlus = document.querySelector('#adults-plus');
 adultsPlus.addEventListener('click', () => {
     let adults = document.querySelector('#adults');
-    let res = addGuest(adults);
+    let res = addGuest(adults,6);
     adults.innerHTML = res;
 });
 
@@ -43,14 +43,14 @@ adultsPlus.addEventListener('click', () => {
 let childMinus = document.querySelector('#children_minus');
 childMinus.addEventListener('click', () => {
     let children = document.querySelector('#children');
-    let res = delGuest(children);
+    let res = delGuest(children,0);
     children.innerHTML = res;
 });
 
 let childPlus = document.querySelector('#children_plus');
 childPlus.addEventListener('click', () => {
     let children = document.querySelector('#children');
-    let res = addGuest(children);
+    let res = addGuest(children,8);
     children.innerHTML = res;
 });
 
@@ -58,14 +58,14 @@ childPlus.addEventListener('click', () => {
 let babyMinus = document.querySelector('#baby_minus');
 babyMinus.addEventListener('click', () => {
     let baby = document.querySelector('#baby');
-    let res = delGuest(baby);
+    let res = delGuest(baby,0);
     baby.innerHTML = res;
 });
 
 let babyPlus = document.querySelector('#baby_plus');
 babyPlus.addEventListener('click', () => {
     let baby = document.querySelector('#baby');
-    let res = addGuest(baby);
+    let res = addGuest(baby,3);
     baby.innerHTML = res;
 });
 
@@ -90,7 +90,7 @@ guestOK.addEventListener('click', () => {
     let baby = Number(document.querySelector('#baby').innerHTML);
 
     let total = document.querySelector('#guests');
-        if (adult === 0 & (child !== 0 || baby !== 0)) {
+        /*if (adult === 0 & (child !== 0 || baby !== 0)) {
             total.innerHTML = "Детей без взрослых не заселяем";
             total.classList.add('warning');
         }
@@ -99,7 +99,9 @@ guestOK.addEventListener('click', () => {
             content = ending(adult,child);
             total.innerHTML = content;
         }
-        
+        */
+        content = ending(adult,child);
+        total.innerHTML = content;
 
     let gChoise = document.querySelector('.guest__choise');
     gChoise.classList.toggle('active');
@@ -176,3 +178,115 @@ for(let i=0; i<likeValues.length; i++) {
         likeValues[i].classList.toggle('active');
     })
 }
+
+/*----------------------------------------------------------------
+    Choise room amenities 
+-----------------------------------------------------------------*/
+let roomBtn = document.querySelector('#amenities__button');
+roomBtn.addEventListener('click', () => {
+    event.preventDefault();
+    let choise = document.querySelector('.romm__choise');
+    choise.classList.toggle('active');
+})
+
+//bedrooms
+let bedroomsMinus = document.querySelector('#bedrooms-minus');    
+bedroomsMinus.addEventListener('click', () => {
+    let bedrooms = document.querySelector('#bedrooms');
+    let res = delGuest(bedrooms,1);
+    bedrooms.innerHTML = res;
+});
+
+let bedroomsPlus = document.querySelector('#bedrooms-plus');
+bedroomsPlus.addEventListener('click', () => {
+    let bedrooms = document.querySelector('#bedrooms');
+    let res = addGuest(bedrooms,2);
+    bedrooms.innerHTML = res;
+});
+
+
+//beds
+let bedsMinus = document.querySelector('#beds_minus');
+bedsMinus.addEventListener('click', () => {
+    let beds = document.querySelector('#beds');
+    let res = delGuest(beds,1);
+    beds.innerHTML = res;
+});
+
+let bedsPlus = document.querySelector('#beds_plus');
+bedsPlus.addEventListener('click', () => {
+    let beds = document.querySelector('#beds');
+    let res = addGuest(beds,4);
+    beds.innerHTML = res;
+});
+
+//bathrooms
+let bathroomsMinus = document.querySelector('#bathrooms_minus');
+bathroomsMinus.addEventListener('click', () => {
+    let bathrooms = document.querySelector('#bathrooms');
+    let res = delGuest(bathrooms,0);
+    bathrooms.innerHTML = res;
+});
+
+let bathroomsPlus = document.querySelector('#bathrooms_plus');
+bathroomsPlus.addEventListener('click', () => {
+    let bathrooms = document.querySelector('#bathrooms');
+    let res = addGuest(bathrooms,2);
+    bathrooms.innerHTML = res;
+});
+
+
+//function
+function endingRoom(a,c) {
+    text = a;
+    if (a == 1)  {
+        text += ' спальня, ';
+    } else {
+        text += ' спальни, ';
+    }
+
+    if (c == 1)  {
+        text += c+' кровать ...';
+    } else {
+        text += c+' кровати ...';
+    }
+
+    return text;
+}
+
+let roomOK = document.querySelector('.room__ok');
+roomOK.addEventListener('click', () => {
+    event.preventDefault();
+    let bedrooms = Number(document.querySelector('#bedrooms').innerHTML);
+    let beds = Number(document.querySelector('#beds').innerHTML);
+    let bathrooms = Number(document.querySelector('#bathrooms').innerHTML);
+
+        let total = document.querySelector('#amenities');
+        content = endingRoom(bedrooms,beds);
+        total.innerHTML = content;
+    
+    let rChoise = document.querySelector('.romm__choise');
+    rChoise.classList.toggle('active');
+});
+
+let rclear = document.querySelector('.room__clear');
+rclear.addEventListener('click', () => {
+    let pole = document.querySelector('#amenities');
+    pole.innerHTML = '2 спальни, 2 кровати...';
+    document.querySelector('#bedrooms').innerHTML = '2';
+    document.querySelector('#beds').innerHTML = '2';
+    document.querySelector('#bathrooms').innerHTML = '0';
+
+    let rChoise = document.querySelector('.romm__choise');
+    rChoise.classList.toggle('active');
+})
+
+
+/*----------------------------------------------------------------
+    Choise room checkbox
+-----------------------------------------------------------------*/
+let btnRoom = document.querySelector('#arrow-switch');
+btnRoom.addEventListener('click', ()=> {
+    document.querySelector('#arrow-switch').classList.toggle('active');
+    document.querySelector('#checkbox__room').classList.toggle('active');
+})
